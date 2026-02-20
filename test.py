b021476a521d50a4cc5d92c1f3266fd5b4295483
Asad <asadullah.work211@gmail.com>
2026-02-20 18:43:25 +0500

    Address

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 class Person:
-    def __init__(self,name,CNIC,gender):
+    def __init__(self,name,CNIC,gender,address):
         self.name = name
         self.CNIC = CNIC
         self.gender = gender
+        self.address = address
 
     def display(self):
         print(f"Name: {self.name}")
         print(f"CNIC = {self.CNIC}")
         print(f"Gender = {self.gender}")
+        print(f"Address: {self.address}")
 
 
-man = Person("Asad",35,'m')
+man = Person("Asad",35,'m',"H#43/9")
 man.display()
 
